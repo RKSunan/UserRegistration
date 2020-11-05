cc737b4b7edc8ba4a79cb9d263ec0f5c6d9429ef
RKSunan <rajkumarisunanda@gmail.com>
2020-11-05 15:07:43 +0530

    Valid Email

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -9,11 +9,20 @@ shopt -s extglob
 #else
 #	echo "Invalid Name"
 #fi
-echo "Enter Last Name"
-read lastName
-if [[ $lastName =~ ^[A-Z][a-z]{2,} ]]
+#echo "Enter Last Name"
+#read lastName
+#if [[ $lastName =~ ^[A-Z][a-z]{2,} ]]
+#then
+#	echo "Valid name"
+#else
+#	echo "Invalid Name"
+#fi
+echo  "Enter email"
+read email
+emailpat="^[a-zA-Z]+([.][a-zA-Z0-9]+)*[@][a-Z]+[.][a-z]+([.][a-z]+)*$"
+if [[ $email =~ $emailpat ]]
 then
-	echo "Valid name"
+echo "valid"
 else
-	echo "Invalid Name"
+	echo "Invalid"
 fi
