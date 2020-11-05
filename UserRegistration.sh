f4ab54a1016f01020cf69eb60946cecefd7e5353
RKSunan <rajkumarisunanda@gmail.com>
2020-11-05 13:31:40 +0530

    Last Name Entry

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,9 +1,18 @@
 #! /bin/bash
 echo "User Registration System needs to ensure all validations are in place during the User Entry"
-echo  "Enter the first name"
-read name
-if [[ $name =~  ^[A-Z]{1}[A-Za-z]{2,}$ ]]
-then 
+shopt -s extglob
+#echo  "Enter the first name"
+#read name
+#if [[ $name =~  ^[A-Z]{1}[A-Za-z]{2,}$ ]]
+#then 
+#	echo "Valid name"
+#else
+#	echo "Invalid Name"
+#fi
+echo "Enter Last Name"
+read lastName
+if [[ $lastName =~ ^[A-Z][a-z]{2,} ]]
+then
 	echo "Valid name"
 else
 	echo "Invalid Name"
